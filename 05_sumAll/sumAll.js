e48537e080aefb7bf00e7d80d6c52998c89dfe63
timbydev <skepticalsec@gmail.com>
2023-05-13 19:54:01 +0000

    execise 5 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+
+	let sum;
+
+	if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+		sum = 'ERROR';
+	} else if (num1 >= 0 && num1 >= num2) {
+
+		sum = 0;
+		for (let i = num1; i >= num2; i--) {
+			sum += i;
+		}
+	} else {
+		sum = 0;
+		for (let i = num2; i >= num1; i--) {
+			sum += i;
+		}
+
+	}
+
+	return sum;
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
