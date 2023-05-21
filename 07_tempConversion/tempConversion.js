b219d35423ee1e6cc8eaa36b810fe5f28571d274
timbydev <skepticalsec@gmail.com>
2023-05-21 02:05:53 +0000

    temperature converter

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  let c = Math.round((deg - 32) * (5/9) * 10) / 10;
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  let f = Math.round(((deg * 9) / 5 + 32) * 10) / 10; 
+  return f;
 };
 
 // Do not edit below this line
@@ -9,3 +13,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+console.log(convertToCelsius(100));
